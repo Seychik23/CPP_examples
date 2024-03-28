ca9dc5818123b603357b8ed04524d1c85dd50e52
Timofey Mukhin <1132227119@pfur.ru>
2024-03-28 14:16:23 +0300

    Add files

---
+++ Primery_programm_C++_Glava_3/Example03_02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n,*p,**q;
+p=&n;
+q=&p;
+n=100;
+(*p)+=5;
+(**q)--;
+cout<<n<<"\n";
+cout<<*p<<"\n";
+cout<<**q<<"\n";
+cout<<p<<"\n";
+cout<<q<<"\n";
+return 0;
+}
---
+++ Primery_programm_C++_Glava_3/Example03_05.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n[10];
+for(int i=0;i<10;i++){
+   n[i]=rand() % 10;
+   cout<<n[i]<<" ";}
+cout<<"\n";
+return 0;
+}
